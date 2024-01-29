2c2824cc87906fced7677e958f02212c3157043c
Ivan Slobodiuk <ivanslo@yahoo.com.ar>
2024-01-29 17:15:00 +0000

    hmm

--- example/perf-test/items-list.spec.ts
+++ example/perf-test/items-list.spec.ts
@@ -4,15 +4,16 @@ import { reset } from '@loveholidays/preact-perf-metrics';
 const RUNNING_URL = 'http://localhost:5173/items-list';
 
 test.describe('Elements Rendered', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(RUNNING_URL);
-  });
   test('ListItem as Function', async ({ page }) => {
+    await page.goto(RUNNING_URL);
+    await page.getByRole('button', { name: 'as-inline-function Next' }).waitFor();
     await reset(page);
     await page.getByRole('button', { name: 'as-inline-function Next' }).click();
     await expect(page).toPerform({ elementsUnmounted: 18 });
   });
   test('ListItem as Component', async ({ page }) => {
+    await page.goto(RUNNING_URL);
+    await page.getByRole('button', { name: 'as-component Next' }).waitFor();
     await reset(page);
     await page.getByRole('button', { name: 'as-component Next' }).click();
     await expect(page).toPerform({ elementsUnmounted: 0 });
